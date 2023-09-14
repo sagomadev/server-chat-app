30b49be5f1cb68620339fd9642efdacb8657c5b1
Samuel Gómez <sagomadev@gmail.com>
2023-09-14 13:10:32 +0200

    Add socketIO to the project

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+//👇🏻 New imports
+const http = require("http").Server(app);
+const cors = require("cors");
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+app.use(cors());
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+});
+
 app.get("/api", (req, res) => {
   res.json({
     message: "Hello world",
   });
 });
 
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
